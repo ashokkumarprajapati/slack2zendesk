b98d9f0ce47a4f3072110241071b57018006951f
ashok kumar <ashokkumarprajapati@gmail.com>
2015-08-25 14:15:42 +0530

    added approval logic.

--- web/slack2zendesk.php
+++ web/slack2zendesk.php
@@ -8,47 +8,47 @@ $pd_subdomain = getenv('PAGERDUTY_SUBDOMAIN');
 $pd_api_token = getenv('PAGERDUTY_API_TOKEN');
 $debug = getenv('DEBUG_ENABLED');
 
-if ($messages) foreach ($messages->messages as $webhook) {
-  $webhook_type = $webhook->type;
-  $incident_id = $webhook->data->incident->id;
-  $ticket_id = $webhook->data->incident->trigger_summary_data->extracted_fields->ticket_id;
-  $ticket_url = $webhook->data->incident->html_url;
-  $pd_requester_id = $webhook->data->incident->assigned_to_user->id;
+if ($messages) {
+  $channel_name = $messages->channel_name;
+  $user_id = $messages->user_id;
+  $requester_name = $messages->user_name;
+  $text = $messages->text;
+  $trigger_type = $messages->trigger_word;
   if ($debug == "true"){
-     error_log("ticket_id to take action :" .$ticket_id);
+     error_log("message recieved from slack: channel=".$channel_name ."username=".$requester_name." message=".$text);
   }
-  switch ($webhook_type) {
-    case "incident.trigger":
+  switch ($trigger_type) {
+    case "@change":
       $verb = "triggered";
       //Remove the pd_integration tag in Zendesk to eliminate further updates
-      $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets/$ticket_id/tags.json";
-      $data = array('tags'=>array('pd_integration'));
+      $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets.json";
+      $title = strstr($text,"@change");
+      $data = array('ticket' => array( 
+ 		 'group\_id' => 24712511,    
+ 		 'subject' => "Change :".$text,  
+ 		 'comment' => $text . "\n\n Created on behalf of:".  
+ 		)  
+            );
       $data_json = json_encode($data);
-      $status_code = http_request($url, $data_json, "DELETE", "basic", $zd_username, $zd_api_token);
+      $status_code = http_request($url, $data_json, "POST", "basic", $zd_username, $zd_api_token);
       break;
-    case "incident.acknowledge":
+    case "@approved":
       $verb = "acknowledged ";
+      $ticket_id = strstr($text,"@approved ");
+      $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets/$ticket_id.json";
+      $data = array('ticket'=>array('comment'=>array('public'=>'false','body'=>"Approved by $requester_name")));
+      $data_json = json_encode($data);
+      $status_code = http_request($url, $data_json, "PUT", "basic", $zd_username, $zd_api_token);
+      if ($status_code != "200") {
+          //If we did not POST correctly to Zendesk, we'll add a note to the ticket, as long as it was a triggered or acknowledged ticket.
+      	  error_log("Error while trying to approve ticket in zendesk");
+	}      
       break;
-    case "incident.resolve":
+    case "@ticket":
       $verb = "resolved";
       break;
     default:
       continue 2;
-  }
-  //Update the Zendesk ticket when the incident is acknowledged or resolved.
-  $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets/$ticket_id.json";
-
-  $data = array('ticket'=>array('comment'=>array('public'=>'false','body'=>"This ticket has been $verb in PagerDuty.  To view the incident, go to $ticket_url.")));
-  $data_json = json_encode($data);
-
-  $status_code = http_request($url, $data_json, "PUT", "basic", $zd_username, $zd_api_token);
-
-  if ($status_code != "200" && $verb != "resolved") {
-    //If we did not POST correctly to Zendesk, we'll add a note to the ticket, as long as it was a triggered or acknowledged ticket.
-    $url = "https://$pd_subdomain.pagerduty.com/api/v1/incidents/$incident_id/notes";
-    $data = array('note'=>array('content'=>'The Zendesk ticket was not updated properly.  Please try again.'),'requester_id'=>"$pd_requester_id");
-    $data_json = json_encode($data);
-    http_request($url, $data_json, "POST", "token", $pd_username, $pd_api_token);
   }
 }
 function http_request($url, $data_json, $method, $auth_type, $username, $token) {
