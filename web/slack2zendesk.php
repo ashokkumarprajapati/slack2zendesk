f7802906f366e76f5d06b724fdf412f943c878ea
ashok kumar <ashokkumarprajapati@gmail.com>
2015-08-26 20:34:55 +0530

    configurable slack token & @ticket feature.

--- web/slack2zendesk.php
+++ web/slack2zendesk.php
@@ -3,23 +3,22 @@ $zd_subdomain = getenv('ZENDESK_SUBDOMAIN');
 $zd_username = getenv('ZENDESK_USERNAME');
 $zd_api_token = getenv('ZENDESK_API_TOKEN');
 $debug = getenv('DEBUG_ENABLED');
-$slack_token = "cd9PEhQSUzJzYVjHPAYmLNSN";
+$slack_token = getenv('ZENDESK_API_TOKEN');
+$channel_name = $_POST["channel_name"];
+$user_id = $_POST["user_id"];
+$requester_name = $_POST["user_name"];
+$text = $_POST["text"];
+$token = $_POST["token"];
+$trigger_type = $_POST["trigger_word"];
 
-  $channel_name = $_POST["channel_name"];
-  $user_id = $_POST["user_id"];
-  $requester_name = $_POST["user_name"];
-  $text = $_POST["text"];
-  $token = $_POST["token"];
-  $trigger_type = $_POST["trigger_word"];
-  error_log("message recieved from slack: channel=".$channel_name ."username=".$requester_name." message=".$text);
-  if ($debug == "true"){
+if ($debug == "true"){
      error_log("message recieved from slack: channel=".$channel_name ."username=".$requester_name." message=".$text);
-  }
-  if ($slack_token != $token){
+}
+if ($slack_token != $token){
      error_log("This invalid request as it doesn't have correct token.");
      return 200;
-  }
-  switch ($trigger_type) {
+}
+switch ($trigger_type) {
     case "@change":
       $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets.json";
       $title = explode("@change ",$text)[1];
@@ -43,7 +42,6 @@ $slack_token = "cd9PEhQSUzJzYVjHPAYmLNSN";
       $data_json = json_encode($data);
       list($status_code,$response) = http_request($url, $data_json, "PUT", "basic", $zd_username, $zd_api_token);
       if ($status_code != "200") {
-      	  //error_log("Error while trying to approve ticket in zendesk");
           $slack_response = array('text' => "Could not approve ticket in zendesk. Please check ticket number. It should be like @approved <Ticket Number>}");
           echo json_encode($slack_response);
           error_log($response);
@@ -53,6 +51,18 @@ $slack_token = "cd9PEhQSUzJzYVjHPAYmLNSN";
       echo json_encode($slack_response);
       break;
     case "@ticket":
+      $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets.json";
+      $title = explode("@change ",$text)[1];
+      $data = array('ticket' => array( 
+                  'group_id' => 24294541,    
+                  'subject' => $title,  
+                  'comment' => $text . "\n\n Created on behalf of:".$requester_name) 
+              );
+      $data_json = json_encode($data);
+      list($status_code,$response) = http_request($url, $data_json, "POST", "basic", $zd_username, $zd_api_token);
+      $ticket_id = json_decode($response)->ticket->id;
+      $slack_response = array('text' => "Zendesk ticket#.$ticket_id has been created and assigned to DevOps. \n Link : https://$zd_subdomain.zendesk.com/agent/tickets/.$ticket_id");
+      echo  json_encode($slack_response);
       break;
     default:
       continue 2;
@@ -63,8 +73,7 @@ function http_request($url, $data_json, $method, $auth_type, $username, $token) 
   if ($auth_type == "token") {
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json),"Authorization: Token token=$token"));
     curl_setopt($ch, CURLOPT_HTTPAUTH);
-  }
-  else if ($auth_type == "basic") {
+  } else if ($auth_type == "basic") {
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($ch, CURLOPT_USERPWD, "$username/token:$token");
